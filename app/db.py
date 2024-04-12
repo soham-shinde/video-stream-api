914d74b3e416fb910ef921a079cde7882b6229b1
Soham Shinde <shindesoham700@gmail.com>
2024-04-13 01:26:02 +0530

    qq

--- app/db.py
+++ app/db.py
@@ -2,4 +2,5 @@ from pymongo import MongoClient
 
 def get_database_client():
     client = MongoClient("mongodb+srv://vitepen266:8haGD2QnI68v1m0W@cluster0.cahjjep.mongodb.net/")
+    print(1)
     return client["video-stream"]
